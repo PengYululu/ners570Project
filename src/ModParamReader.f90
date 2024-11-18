c7f468a4f89ea31aad2f70dcdd271b538d050e26
qplazmfree <qplazmfree@qplazmfrees-MacBook-Pro.local>
2024-11-18 14:06:59 -0500

    Modules to 1. declare, and 2. Read parameters.

---
+++ src/ModParamReader.f90
@@ -0,0 +1,168 @@
+module ModParamReader
+
+    use ModParameters, only: Mx,My,Mz,nBlocks_global,&
+        nx,ny,nz,ng,x_range_global,y_range_global,z_range_global,&
+        Xi,TypeDiffusion,Diffusion_h,UseGravity,TypeGas,&
+        TypeSuperAdiabaticity,SuperAdiabaticity,UseRSST
+
+    ! Generic interface to read a parameter from a line
+    !
+    ! Usage:
+    !       call read_var(line,parameter)
+    !
+    ! Keywords:
+    !       line        :   The character type of one line
+    !       parameter   :   The target parameter to read into
+
+    contains
+
+    subroutine Read_paramters(filename)
+        implicit none
+        character(len=*),intent(in)     ::  filename            ! Parameter input filename
+        character(len=256)              ::  line,var            ! Which line it is
+        character(len=14)               ::  name_sub='Read_paramters'
+        integer                         ::  unit, ios           ! For reading
+    
+        ! Open the input file
+        unit = 10
+        open(unit, file=filename, status='old', action='read', iostat=ios)
+        if (ios /= 0) then
+            print *, "Error opening file: ", filename
+            stop
+        end if
+    
+        ! Read the file line by line
+        do
+            read(unit, '(A)', iostat=ios) line
+            if (ios /= 0) exit  ! End of file
+    
+            ! If starts with # then enter this command block
+            if (line(1:1) == "#") then
+    
+                ! See which command it is
+                select case (trim(adjustl(line)))
+                case ("#GRAVITY")
+
+                    ! UseGravity
+                    read(unit, *, iostat=ios) var
+                    select case (trim(adjustl(var)))
+                    case("T","t","true","True","TRUE")
+                        UseGravity = .true.
+                    case("F","f","false","False","FALSE")
+                        UseGravity = .false.
+                    case default
+                        write(*,*) "Error from ",name_sub,": Unknown UseGravity=",trim(adjustl(var))
+                        stop 1
+                    end select
+    
+                case ("#DOMAIN")
+                    read(unit, *, iostat=ios) x_range_global(1)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading xmin"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) y_range_global(1)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading ymin"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) z_range_global(1)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading zmin"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) x_range_global(2)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading xmax"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) y_range_global(2)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading ymax"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) z_range_global(2)
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading zmax"
+                        stop 1
+                    end if
+                
+                case ("#BLOCKS")
+                    read(unit, *, iostat=ios) nx
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading nx"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) ny
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading ny"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) nz
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading nz"
+                        stop 1
+                    end if
+                    read(unit, *, iostat=ios) ng
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading ng"
+                        stop 1
+                    end if
+
+                case("#GAS")
+                    read(unit, *, iostat=ios) var
+                    select case (trim(adjustl(var)))
+                    case("Ideal",'ideal','IDEAL')
+                        TypeGas="Ideal"
+                    case default
+                        write(*,*) "Error from ",name_sub,": Unknown TypeGas=",trim(adjustl(var))
+                        stop 1
+                    end select
+
+                case("#RSST")
+                    read(unit, *, iostat=ios) var
+                    select case (trim(adjustl(var)))
+                    case("T","t","true","True","TRUE")
+                        UseRSST = .true.
+                    case("F","f","false","False","FALSE")
+                        UseRSST = .false.
+                    case default
+                        write(*,*) "Error from ",name_sub,": Unknown UseRSST=",trim(adjustl(var))
+                        stop 1
+                    end select
+
+                case("#DIFFUSION")
+                    read(unit, *, iostat=ios) var
+                    select case (trim(adjustl(var)))
+                    case("artificial","ARTIFICIAL","Aritificial")
+                        TypeDiffusion="Aritificial"
+                    case default
+                        write(*,*) "Error from ",name_sub,": Unknown TypeDiffusion=",trim(adjustl(var))
+                        stop 1
+                    end select
+
+                    read(unit, *, iostat=ios) Diffusion_h
+                    if (ios/=0) then
+                        write(*,*) "Error from ",name_sub,": Error reading Diffusion_h"
+                        stop 1
+                    end if
+
+                case("#SUPERADIABATICITY")
+                    read(unit, *, iostat=ios) var
+                    select case (trim(adjustl(var)))
+                    case("artificial","ARTIFICIAL","Aritificial")
+                        TypeDiffusion="Aritificial"
+                    case default
+                        write(*,*) "Error from ",name_sub,": Unknown TypeDiffusion=",trim(adjustl(var))
+                        stop 1
+                    end select
+
+                end select
+            end if
+        end do
+    
+        ! Close the file
+        close(unit)
+    end subroutine Read_paramters
+
+end module ModParamReader
--- src/ModParameters.f90
+++ src/ModParameters.f90
@@ -1,10 +1,34 @@
 Module ModParameters
 
+    ! Simulation box and grid
     integer             ::  Mx,My,Mz                !   Number of blocks in each direction.
+    integer             ::  nBlocks_global          !   Total number of blocks.
+    integer             ::  nBlocks                 !   Local number of blocks.
+    integer             ::  iBlock_range            !   Indice of local blocks.
     integer             ::  nx,ny,nz                !   Number of grid points in one block
                                                     !   for each direction.
-    integer             ::  x_range_global(2),&     !   The xyz range of the whole blo
+    integer             ::  ng                      !   Number of ghost cells layers.
+    real                ::  x_range_global(2),&     !   The xyz range of the whole box.
                             y_range_global(2),&
                             z_range_global(2)
+    
+    ! RSST
+    logical             ::  UseRSST
+    real                ::  Xi                      !   Xi for reduced sound speed
 
+    ! Diffusion
+    character(len=100)  ::  TypeDiffusion           !   If use gravity
+    real                ::  Diffusion_h             !   Used in artificial Diffusion
+    
+    ! Gravity
+    logical             ::  UseGravity              !   If use gravity
+
+    ! Gas
+    character(len=100)  ::  TypeGas                 !   Ideal or nonIdeal
+    real                ::  GasGamma=5./3.          !   Gamma of the ideal gas
+    
+    ! Superadiabaticity
+    character(len=100)  ::  TypeSuperAdiabaticity   !   Type of superadiavaticity
+    real                ::  SuperAdiabaticity
+    
 end module ModParameters
