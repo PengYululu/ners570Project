c053c0db7d92b4353e219516205eb61bf34af7da
qplazmfree <qplazmfree@qplazmfrees-MacBook-Pro.local>
2024-11-19 14:22:20 -0500

    fix bug

--- src/ModJobAssigner.f90
+++ src/ModJobAssigner.f90
@@ -5,26 +5,20 @@ module ModJobAssigner
 contains
 
     ! Get the range of local iBlocks
-    subroutine GetRange(irank, iBlock, nBlockTotal, nRanks, iStart, iEnd)
-        integer, intent(in) :: irank, nBlockTotal, nRanks
-        integer, intent(out) :: iBlock, iStart, iEnd
+
+    subroutine GetRange(irank, nBlockTotal, nRanks, iBlock_range)
+        integer, intent(in)     ::  irank               !   I of rank
+        integer, intent(in)     ::  nBlockTotal         !   Total N of blocks
+        integer, intent(in)     ::  nRanks              !   Total N of ranks
+        integer, intent(out)    ::  iBlock_range(2)     !   Local range of iBlocks
 
         integer :: nBlockPerRank
 
         nBlockPerRank = nBlockTotal/nRanks
-        iStart = irank * nBlockPerRank + 1
-        iEnd = (irank + 1 ) * nBlockPerRank
-
-        if (irank == nRanks - 1) then
-            iEnd = nBlockTotal
-        endif 
-
-        iBlock = irank + 1
-
+        iBlock_range=[irank * nBlockPerRank + 1,(irank + 1 ) * nBlockPerRank]
     end subroutine GetRange
 
-    ! Use iBlock to find which iRank
-    ! it belongs to
+    ! Use iBlock to find which iRank it belongs to
 
     subroutine GetiRank(nBlockTotal, nRanks, iBlock, iRank)
         integer, intent(in)     ::  nBlockTotal     !   N of Blocks total
@@ -36,7 +30,6 @@ contains
 
         nBlockPerRank = nBlockTotal / nRanks
         iRank = (iBlock - 1) / nBlockPerRank
-    
     end subroutine GetiRank
 
 end module ModJobAssigner
