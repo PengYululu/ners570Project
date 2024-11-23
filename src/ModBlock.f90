96eecb82bcabd83e31ab92afa9a8919c08724c0d
qplazmfree <qplazmfree@qplazmfrees-MacBook-Pro.local>
2024-11-23 16:32:41 -0500

    Minor fixes

--- src/ModBlock.f90
+++ src/ModBlock.f90
@@ -24,7 +24,7 @@ module ModBlock
     end type BlockType
 
 contains
-    subroutine Init_OneBlock(block, iBlock, Mx, My, Mz, nx, ny, nz, ng, nvars, x_range_global, y_range_global, z_range_global)
+    subroutine Init_OneBlock(block1, iBlock, Mx, My, Mz, nx, ny, nz, ng, nvars, x_range_global, y_range_global, z_range_global)
         ! Initialize a block based on MPI rank and block configuration
         !
         ! Arguments:
@@ -35,17 +35,17 @@ contains
         ! ng      - Number of ghost cells
         ! nvars   - Number of variables stored per grid point
         implicit none
-        type(BlockType), target :: block
+        type(BlockType), target :: block1
         integer, intent(in) :: iBlock, Mx, My, Mz
         integer, intent(in) :: nx, ny, nz, ng, nvars
-        real, intent(in) :: x_range_global, y_range_global, z_range_global
+        real, intent(in) :: x_range_global(2), y_range_global(2), z_range_global(2)
         integer :: ix, iy, iz
         
         ! Store grid dimensions
-        block%nx = nx
-        block%ny = ny
-        block%nz = nz
-        block%ng = ng
+        block1%nx = nx
+        block1%ny = ny
+        block1%nz = nz
+        block1%ng = ng
         
         ! Calculate block indices (convert rank to i,j,k indices)
         ! rank = ix + Mx*(iy-1) + (Mx*My)*(iz-1)
@@ -53,24 +53,24 @@ contains
         iy = (mod(iBlock - 1, Mx * My) + 1) / Mx + 1
         ix = mod(iBlock - 1, Mx) + 1
         
-        block%x_range(1) = (ix - 1) * nx * (x_range_global / real(Mx * nx)) + 1
-        block%x_range(2) = ix * nx * (x_range_global / real(Mx * nx))
-        block%y_range(1) = (iy - 1) * ny * (y_range_global / real(My * ny)) + 1
-        block%y_range(2) = iy * ny * (y_range_global / real(My * ny))
-        block%z_range(1) = (iz - 1) * nz * (z_range_global / real(Mz * nz)) + 1
-        block%z_range(2) = iz * nz * (z_range_global / real(Mz * nz))
+        block1%x_range(1) = x_range_global(1) * (Mx-ix+1)   + x_range_global(2) * (ix-1)
+        block1%x_range(2) = x_range_global(1) * (Mx-ix)     + x_range_global(2) * ix
+        block1%y_range(1) = y_range_global(1) * (My-iy+1)   + y_range_global(2) * (iy-1)
+        block1%y_range(2) = y_range_global(1) * (My-iy)     + y_range_global(2) * iy
+        block1%z_range(1) = z_range_global(1) * (Mz-iz+1)   + z_range_global(2) * (iz-1)
+        block1%z_range(2) = z_range_global(1) * (Mz-iz)     + z_range_global(2) * iz
 
-        block%dx = (x_range_global / real(Mx * nx))
-        block%dy = (y_range_global / real(My * ny))
-        block%dz = (z_range_global / real(Mz * nz)) 
+        block1%dx = (block1%x_range(2) - block1%x_range(1))/nx
+        block1%dy = (block1%y_range(2) - block1%y_range(1))/ny
+        block1%dz = (block1%z_range(2) - block1%z_range(1))/nz
 
-        block%if_bottom = (iz == 1)
-        block%if_top = (iz == Mz)
+        block1%if_bottom = (iz == 1)
+        block1%if_top = (iz == Mz)
         
         ! Allocate memory for values array including ghost cells
-        if (allocated(block%values)) deallocate(block%values)
-        allocate(block%values(-ng+1:nx+ng, -ng+1:ny+ng, -ng+1:nz+ng, nvars))
-        block%values = 0.0
+        if (allocated(block1%values)) deallocate(block1%values)
+        allocate(block1%values(-ng+1:nx+ng, -ng+1:ny+ng, -ng+1:nz+ng, nvars))
+        block1%values = 0.0
     end subroutine Init_OneBlock
     
     subroutine Clean_OneBlock(block)
@@ -85,13 +85,14 @@ contains
         implicit none
         type(BlockType), dimension(:), intent(inout) :: blocks
         integer, intent(in) :: nBlocks, Mx, My, Mz, nx, ny, nz, ng, nvars
-        real, intent(in) :: x_range_global, y_range_global, z_range_global
+        real, intent(in) :: x_range_global(2), y_range_global(2), z_range_global(2)
         integer :: iBlock
 
         do iBlock = 0, nBlocks-1
             call Init_OneBlock(blocks(iBlock+1), iBlock, Mx, My, Mz, nx, ny, nz, ng, nvars, &
                 x_range_global, y_range_global, z_range_global)
         end do
+        call All_Block_rho0P0(blocks, nBlocks)
     end subroutine Init_AllBlocks
 
     subroutine Clean_AllBlocks(blocks, nBlocks)
@@ -105,41 +106,36 @@ contains
         end do
     end subroutine Clean_AllBlocks
 
-    subroutine Init_Block_rho0P0(block, iBlock, Mx, My, Mz, GasGamma, z_range_global)
+    subroutine Init_Block_rho0P0(block1, GasGamma)
         implicit none
-        type(BlockType), intent(inout) :: block
-        integer, intent(in) :: iBlock, Mx, My, Mz
-        real, intent(in) :: GasGamma, z_range_global
-        integer :: i, j, k, m, iz
-        real :: z_start, z_end, z_local, z_global
+        type(BlockType), intent(inout) :: block1
+        real, intent(in) :: GasGamma
+        integer ::  i, j, k
+        real    ::  m,z
 
-        iz = iBlock / (Mx * My) + 1
-        z_start = block%z_range(1)
-        z_end = block%z_range(2)
         m = 1.0 / (GasGamma - 1.0)
 
-        do k = -block%ng+1, block%nz + block%ng
-            do j = -block%ng+1, block%ny + block%ng
-                do i = -block%ng+1, block%nx + block%ng
-                    z_local = z_start + (k-1)*block%dz
-                    z_global = z_local / z_range_global
-                    block%rho0(i,j,k) = (1 - z_global/(m+1))**m
-                    block%P0(i,j,k) = (1 - z_global/(m+1))**(m+1)
+        do k = -block1%ng+1, block1%nz + block1%ng
+            do j = -block1%ng+1, block1%ny + block1%ng
+                do i = -block1%ng+1, block1%nx + block1%ng
+                    z = block1%z_range(1) + (k-0.5)*block1%dz
+                    block1%rho0(i,j,k) = (1 - z/(m+1))**m
+                    block1%P0(i,j,k) = (1 - z/(m+1))**(m+1)
                 end do
             end do
         end do
     
     end subroutine Init_Block_rho0P0
 
-    subroutine All_Block_rho0P0(blocks, nBlocks, Mx, My, Mz, GasGamma, z_range_global)
+    subroutine All_Block_rho0P0(blocks, nBlocks)
+        use ModParameters,only  :   GasGamma
         implicit none
         type(BlockType), dimension(:), intent(inout) :: blocks
-        integer, intent(in) :: nBlocks, Mx, My, Mz
-        real, intent(in) :: GasGamma, z_range_global
+        integer, intent(in) :: nBlocks
         integer :: iBlock
 
         do iBlock = 0, nBlocks-1
-            call Init_Block_rho0P0(blocks(iBlock+1), iBlock, Mx, My, Mz, GasGamma, z_range_global)
+            call Init_Block_rho0P0(blocks(iBlock+1), GasGamma)
         end do
     end subroutine All_Block_rho0P0
 
